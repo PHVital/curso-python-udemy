84ebc78c8e7feb9daac30bc2a133fd2915a27f0a
PHVital <vital.ph.g@gmail.com>
2025-04-10 19:49:25 -0300

    Exercicio POO sobre banco

--- sistema-bancario-pilares-poo.py
+++ sistema-bancario-pilares-poo.py
@@ -1,15 +1,14 @@
 from abc import ABC, abstractmethod
 
 class Conta(ABC):
-    limite_extra = 500
-
     def __init__(self, agencia, numero_conta, saldo=0):
         self.agencia = agencia
         self.conta = numero_conta
         self.saldo = saldo
 
-    def deposito(self, valor):
+    def depositar(self, valor):
         self.saldo += valor
+        print(f'Depositado {valor}, seu saldo agora é {self.saldo}')
 
     @abstractmethod
     def sacar(self, valor):
@@ -20,22 +19,31 @@ class Conta(ABC):
 
 
 class ContaCorrente(Conta):
-    def __init__(self, agencia, numero_conta, saldo=0):
+    def __init__(self, agencia, numero_conta, saldo=0, limite=0):
         super().__init__(agencia, numero_conta, saldo)
+        self.limite = limite
 
     def sacar(self, valor):
-        total_disponivel = self.saldo + self.limite_extra
+        # Verifica se o valor solicitado não ultrapassa o saldo + limite
+        total_disponivel = self.saldo + self.limite
         if valor > total_disponivel:
             return 'Saldo insuficiente'
 
         if valor <= self.saldo:
             self.saldo -= valor
         else:
+            # Quando o saldo for insuficiente, o restante é retirado do limite
             restante = valor - self.saldo
             self.saldo = 0
-            self.limite_extra -= restante
+            self.limite -= restante
 
-        return f"Saque de {valor} realizado com sucesso. Saldo atual: {self.saldo}"
+        # Garantir que o limite não seja negativo
+        if self.limite < 0:
+            self.limite = 0
+
+        print(f"Saque de {valor} realizado com sucesso. Saldo atual: {self.saldo}, Limite restante: {self.limite}")
+
+
 
 
 
@@ -49,7 +57,7 @@ class ContaPoupanca(Conta):
         if valor > self.saldo:
             return 'Saldo insuficiente'
         self.saldo -= valor
-        return f"Saque de {valor} realizado com sucesso. Saldo atual: {self.saldo}"
+        print(f"Saque de {valor} realizado com sucesso. Saldo atual: {self.saldo}")
 
 
 
@@ -108,3 +116,19 @@ class Banco:
         if conta.agencia != agencia:
             return False
         return True
+    
+
+cp1 = ContaPoupanca(111, 222)
+cp1.sacar(1)
+cp1.depositar(1)
+cp1.sacar(1)
+cp1.sacar(1)
+print('##')
+cc1 = ContaCorrente(111, 222, 0, 100)
+cc1.sacar(1)
+cc1.depositar(1)
+cc1.sacar(1)
+cc1.sacar(1)
+cc1.sacar(98)
+cc1.sacar(1)
+print('##')
